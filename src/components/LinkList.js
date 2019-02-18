02fada311936d87fddd6ec8c2b515c61c9eaaa0b
Priya <ppuliyurudaiyan@paypal.com>
2019-02-18 08:13:05 -0800

    Apollo Graphql client

---
+++ src/components/LinkList.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Link from './Link';
+
+class LinkList extends Component {
+    render() {
+
+        const linksToRender = [
+            {
+              id: '1',
+              description: 'Prisma turns your database into a GraphQL API 😎',
+              url: 'https://www.prismagraphql.com',
+            },
+            {
+              id: '2',
+              description: 'The best GraphQL client',
+              url: 'https://www.apollographql.com/docs/react/',
+            },
+          ]
+
+        return (
+            <div>
+                {
+                    linksToRender.map(link => { 
+                        return <Link key={link.id} link={link}> </Link>
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+export default LinkList;
